b8d3e2996295f02e9d0bf33448fd96d7eb24fcf0
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-06-21 01:13:45 +0600

    Implement modal functionality and enhance deal card display with dynamic pet data

--- js/app.js
+++ js/app.js
@@ -3,23 +3,39 @@ import * as ui from './ui.js'
 
 export const loadCategories = () => {
     fetch('https://openapi.programming-hero.com/api/peddy/categories')
-    .then(r => r.json())
-    .then(json => ui.displayCategoryButtons(json.categories))
-    .catch(e => console.log("error occurred: ", e))
+        .then(r => r.json())
+        .then(json => ui.displayCategoryButtons(json.categories))
+        .catch(e => console.log("error occurred: ", e))
 }
 
-export async function loadDeals(endpoint = 'pets'){
-    try{
-    const res = await fetch(`https://openapi.programming-hero.com/api/peddy/${endpoint}`)
-    const json = await res.json()
-    // console.log(json);
-    ui.clearBestDeal()
-    const pets = json.pets?json.pets:json.data
-    pets.forEach(pet => {
-        console.log(pet);
-        ui.addBestDealCard(pet)
-    });
-    }catch(err){
-        console.error("Error:::: ", err);
-    }
+export function loadDeals(endpoint = 'pets') {
+    fetch(`https://openapi.programming-hero.com/api/peddy/${endpoint}`)
+        .then(res => res.json())
+        .then((json) => {
+            ui.clearBestDeal()
+            const pets = json.pets ? json.pets : json.data
+            pets.forEach(pet => {
+                console.log(pet);
+                ui.addBestDealCard(pet)
+            });
+            return pets;
+        })
+        .then(pets => {
+            document.querySelectorAll('.modal-btn').forEach((btn, index) => {
+                console.log(btn);
+                btn.addEventListener('click', () => {
+                    ui.showModal(pets[index])
+                })
+            })
+            document.querySelectorAll('.like-btn').forEach((btn, index) => {
+                console.log(btn);
+                btn.addEventListener('click', () => {
+                    ui.addToFav(pets[index])
+                })
+            })
+        })
+
+        .catch((err) => {
+            console.error("Error:::: ", err);
+        })
 }
--- js/ui.js
+++ js/ui.js
@@ -43,20 +43,45 @@ export function addBestDealCard(pet) {
                                     alt="Shoes" />
                             </figure>
                             <div class="card-body">
-                                <h2 class="card-title">Sunny</h2>
-                                <p>Breed: Golden Retriever</p>
-                                <p>Birth: 2024</p>
-                                <p>Gender: Female</p>
-                                <p>Price: 199$</p>
+                                <h2 class="card-title">${pet.pet_name}</h2>
+                                <p>Breed: ${pet.breed}</p>
+                                <p>Birth: ${pet.date_of_birth}</p>
+                                <p>Gender: ${pet.gender}</p>
+                                <p>Price: ${pet.price}$</p>
                                 <div class="card-actions justify-between">
-                                    <button class="btn btn-outline"><img class="size-7" src="https://img.icons8.com/?size=50&id=24816&format=png" alt=""></button>
+                                    <button class="btn like-btn btn-outline"><img class="size-7" src="https://img.icons8.com/?size=50&id=24816&format=png" alt=""></button>
                                     <div>
                                     <button class="btn btn-outline">Adopt</button>
-                                    <button class="btn btn-outline">Details</button>
+                                    <button class="btn modal-btn btn-outline" >Details</button>
                                     </div>
                                 </div>
                             </div>
     `
-
     container.appendChild(card)
+}
+
+export function showModal(pet){
+    document.getElementById('my_modal_1').innerHTML = `
+                <div class="modal-box">
+                <img class='mx-auto' src="${pet.image}">
+            <h3 class="text-lg font-bold">${pet.pet_name}</h3>
+            <p class="py-4">${pet.pet_details}</p>
+            <div class="modal-action">
+                <form method="dialog">
+                    <!-- if there is a button in form, it will close the modal -->
+                    <button class="btn">Close</button>
+                </form>
+            </div>
+        </div>
+    `
+
+    my_modal_1.showModal()
+}
+
+export function addToFav(pet){
+    const image = document.createElement('img')
+    image.src = pet.image
+    image.classList = 'mb-0 p-0'
+
+    document.getElementById('liked').appendChild(image)
 }
